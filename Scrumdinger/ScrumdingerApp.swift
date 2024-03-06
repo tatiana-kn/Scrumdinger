cd617bf65b731b91f6d3e681f9de1199ecbe648c
tatiana-kn <123388284+tatiana-kn@users.noreply.github.com>
2024-03-06 10:47:02 +0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Tia M on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
